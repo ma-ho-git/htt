dc8963c461f19d3b9a87b118df69d3be925d68b3
ma-ho-git <marcus.hopka@googlemail.com>
2024-08-05 10:29:52 +0200

    Ende Kap04

--- swimclub.py
+++ swimclub.py
@@ -1,6 +1,8 @@
 import statistics
+import hfpy_utils
 
 FOLDER = "swimdata/"
+CHARTS = "charts/"
 
 def read_swim_data(filename):
     
@@ -26,13 +28,57 @@ def read_swim_data(filename):
 
     average = statistics.mean(converts)
     # seconds and hundredth
-    sah = round(average/ 100, 2)
+    # sah = round(average/ 100, 2)
     # as string list
-    mins_secs, hundredths = str(sah).split(".")
+    mins_secs, hundredths = f"{(average/100):.2f}".split(".") # str(sah).split(".")
     mins_secs = int(mins_secs) 
     minutes = mins_secs // 60
     seconds = mins_secs - minutes *60
 
-    average_str = str(minutes)+":"+str(seconds)+"."+str(hundredths)
+    average = f"{minutes}:{seconds:0>2}.{hundredths}" 
+    #str(minutes)+":"+str(seconds)+"."+str(hundredths)
 
-    return swimmer, age, distance, stroke, times, average 
+    return swimmer, age, distance, stroke, times, average, converts
+
+def produce_bar_chart(filename):
+    (swimmer, age, distance, stroke, times, avarage, converts) = read_swim_data(filename)
+
+    title = f"{swimmer} (Under {age} ) {distance} {stroke}"
+
+    # start html file
+    html = f"""<!DOCTYPE html> 
+    <html>
+        <head>
+            <title> {title} </title> 
+        </head>
+        <body> 
+            <h3>  {title} </h3>"""
+    
+    # svg-bild
+    from_max = max(converts)
+    svgs = ""
+    times.reverse()
+    converts.reverse()
+
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n],0,from_max,0,350)
+        svgs = svgs + f"""
+        
+            <svg height="30" width="400">
+                <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);"  > 
+            </svg>{t} <br />
+            """
+    # eof    
+    footer = f"""
+                    <p> Average time: {avarage}</p>
+            </body>
+    </html>
+    """
+    page = f"{html}{svgs}{footer}"
+    save_to =f"{CHARTS}{filename.removesuffix(".txt")}.html"
+
+    # generierte seite in html-datei schreiben
+    with open(save_to, "w") as tf: # mit parameter w wird neu geschrieben mit a angehängt
+        print(page, file=tf)
+    
+    return save_to
